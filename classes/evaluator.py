b1325197d55ce2d754f743ca58a1338dec935a12
Jofre Pinheiro <jofrepinheiro@gmail.com>
2018-05-09 00:06:30 -0300

    Evaluator improvement
    Addition of shifted sphere, Rosenbrock and Rastrign functions for benchmarking

--- classes/evaluator.py
+++ classes/evaluator.py
@@ -1,4 +1,40 @@
+import math
+
 class Evaluator:
 
-    def __init__(self):
-        pass
+    def __init__(self, error = 1):
+        self.error = error
+    
+    
+    def shiftedSphere(self, solution, bias = 0 ):
+        "Shifted Sphere function for benchmarking"
+        sum = 0
+        for x in range(0, len(solution)):
+            sum += solution[x] * solution[x]
+        sum += bias
+        return sum <= self.error
+
+
+    def shiftedRosenbrock(self, solution, bias = 0 ):
+        "Shifted Rosenbrock's function for benchmarking."
+
+        sum = 0
+        for x in range(0, len(solution) - 1):
+            sum += (100 * ((solution[x]**2) - solution[x + 1])**2) + (solution[x] - 1)**2
+
+        sum += bias
+
+        return sum <= self.error
+
+    
+    def shiftedRastrign(self, solution, bias = 0):
+        "Shifted Rosenbrock's function for benchmarking."
+
+        sum = 0
+        for x in range(0, len(solution)):
+            sum += solution[x]**2 - (10 * math.cos(2 * math.pi * solution[x]) + 10)
+        
+        sum += bias
+
+        return sum <= self.error
+        
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from classes.solution import Solution
+from classes.evaluator import Evaluator
 
 def main():
-    solution = Solution(100, 20)
-    
+    # Solution handling
+    solution = Solution(5, 5)
     solution.start()
-
     solution.print()
 
-
+    # Evaluation
+    evaluator = Evaluator()
+    print(evaluator.shiftedSphere(solution.content, 0.3))
+    print(evaluator.shiftedRosenbrock(solution.content, 0.3))
+    print(evaluator.shiftedRastrign(solution.content, 0.3))
     
-
 main()
