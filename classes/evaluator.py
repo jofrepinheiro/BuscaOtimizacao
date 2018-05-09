e5cec9d358cf204bc7647562eade50d697733ca4
Jofre Pinheiro <jofrepinheiro@gmail.com>
2018-05-09 14:03:10 -0300

    Created Shifted Griewank benchmarking

--- classes/evaluator.py
+++ classes/evaluator.py
@@ -16,10 +16,10 @@ class Evaluator:
 
 
     def shiftedRosenbrock(self, solution, bias = 0 ):
-        "Shifted Rosenbrock's function for benchmarking."
+        "Shifted Rosenbrock's function for benchmarking"
 
         sum = 0
-        for x in range(0, len(solution) - 1):
+        for x in range(0, len(solution) - 2):
             sum += (100 * ((solution[x]**2) - solution[x + 1])**2) + (solution[x] - 1)**2
 
         sum += bias
@@ -28,12 +28,28 @@ class Evaluator:
 
     
     def shiftedRastrign(self, solution, bias = 0):
-        "Shifted Rosenbrock's function for benchmarking."
+        "Shifted Rosenbrock's function for benchmarking"
 
         sum = 0
-        for x in range(0, len(solution)):
+        for x in range(0, len(solution) - 1):
             sum += solution[x]**2 - (10 * math.cos(2 * math.pi * solution[x]) + 10)
         
         sum += bias
 
         return sum        
+
+
+    def shiftedGriewank(self, solution, bias = 0):
+        "Shifted Griewank's function for benchmarking"
+
+        sum = 0
+        product = 1
+
+        for x in range(0, len(solution) - 1):
+            sum += solution[x]**2 / 4000
+        
+        for y in range(0, len(solution) - 1):
+            product *= math.cos(solution[y] / math.sqrt(y))
+        
+        return sum - product + 1 + bias
+            
